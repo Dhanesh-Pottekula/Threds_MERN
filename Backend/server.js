baf70fba04da755272b0372f12c7293f635ec2cc
dhanesh <dhaneshpk359@gmail.com>
2024-01-06 16:12:53 +0530

    first message

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
   });
 
-
+app.use(express.static('dist'))
 ConnectDB()
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended:true}))
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:5173,
-    proxy:{
-      "/api":{
-        target:"https://threds-mern.vercel.app",
-        changeOrigin:true,
-        secure:false,
-      }
-    }
-  }
-})
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://threds-mern.vercel.app",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["lodash", "axios"], // Put lodash and axios into a separate chunk
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust chunk size limit to 1MB
+  },
+});
