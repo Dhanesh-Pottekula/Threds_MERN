e1ef1cd27cc44ff2f4fdcee1959369cdfb74f089
dhanesh <dhaneshpk359@gmail.com>
2024-01-06 19:01:10 +0530

    deleted vercel

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ import postRoutes from './routes/PostRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import  {v2 as cloudinary} from 'cloudinary';
 import {app, server} from './socket/socket.js'
-import path  from 'path';
+
 dotenv.config()
 
 const port = 8080
