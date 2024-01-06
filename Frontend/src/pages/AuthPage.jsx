d044aeadbe3b5bb7fbc637684273400e29f899f2
dhanesh <dhaneshpk359@gmail.com>
2024-01-06 14:40:46 +0530

    modifyied signupCard component

--- Frontend/src/pages/AuthPage.jsx
+++ Frontend/src/pages/AuthPage.jsx
@@ -2,7 +2,7 @@ import { useRecoilValue } from "recoil";
 import LoginCard from "../components/LoginCard";
 
 import authScreenAtom from "../atoms/authAtom";
-import SignupCard from "../components/SignUpCard";
+import SignupCard from "../components/SignupCard";
 
 const AuthPage = () => {
 	const authScreenState = useRecoilValue(authScreenAtom);
